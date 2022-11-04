9aff0b668171e3e80a51923570152a716962f0b4
vkdnvkjdnvk <115922483+vkdnvkjdnvk@users.noreply.github.com>
2022-11-04 17:00:03 +0530

    Update constant.py

--- ytdlbot/constant.py
+++ ytdlbot/constant.py
@@ -19,7 +19,7 @@ from utils import get_func_queue
 
 
 class BotText:
-    start = "•Welcome to All download bot.🎁\n•Join:-@doppelfod❤️ to use this bot. Type /help for more information"
+    start = "•Welcome to All download bot.🎁\n•Join:-@doppelfod❤️ to use this bot. \n•Type /help for more informationℹ "
 
     help = f"""
 1. This bot should works at all times. If it doesn't, try to send the link again .
